964825ee368c1fa149ca56a66b2bd494dee7fe4d
Iidaaaaaa <iidaaaatomo27@gmail.com>
2024-10-07 11:38:11 +0900

    motion battle

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+import Test from "./Components/test";
+const App = () => {
+  return (
+    <div>
+      <Test />
+    </div>
+  );
+};
+
+export default App;
